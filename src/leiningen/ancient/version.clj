9aefab2b3bdf1ffcd01d1a00a43e2695d38b63a8
Yannick Scherer <yannick.scherer@gmail.com>
2013-06-26 14:49:57 +0200

    Implemented more generic version comparator.

--- src/leiningen/ancient/version.clj
+++ src/leiningen/ancient/version.clj
@@ -2,57 +2,145 @@
       :author "Yannick Scherer"}
   leiningen.ancient.version)
 
-(def ^:private VERSION_REGEX 
-  #"^([0-9]+)\.([0-9]+)(\.([0-9]+))?(\-(.+))?$")
+;; ## Version Vector
+;;
+;; The logic given [here](http://docs.codehaus.org/display/MAVEN/Versioning) is used
+;; to convert a string into a version vector that consists of:
+;; 
+;; - integers
+;; - strings
+;; - subvectors
+;;
+;; A "-" character creates a subvector, a "." splits elements in the current subvector.
+
+(defn- version-split-by-change
+  "Split a given string into char-only and int-only parts."
+  [^String v]
+  (loop [^String v v
+         result []]
+    (if (seq v)
+      (let [[c & rst] v
+            split-rx (if (Character/isDigit c) "[^0-9]" "[0-9]")
+            split-result (.split v split-rx 2)
+            first-part (first split-result)
+            rest-part (.substring v (count first-part))]
+        (recur rest-part (conj result first-part)))
+      result)))
+
+(defn- version-split-component
+  "Split version component on '.' and into char-only and int-only parts."
+  [^String v]
+  (let [by-dot (.split v "\\.")
+        by-change (mapcat version-split-by-change by-dot)]
+    (map 
+      (fn [^String x]
+        (try
+          (Integer/parseInt x)
+          (catch Exception _ 
+            (.toLowerCase x))))
+      by-change)))
+
+(defn- version-split
+  "Split version string into version vector."
+  [^String version]
+  (let [by-dash (seq (.split version "-"))]
+    (reduce
+      (fn [r version-component]
+        (concat
+          (version-split-component version-component)
+          (when r (vector r))))
+      nil
+      (reverse by-dash))))
+
+;; ## Version Comparison
+;;
+;; Comparison is done using the following table:
+;;
+;;         |  Integer  |  String    |   List            |        Nil
+;; ---------------------------------------------------------------------------
+;; Integer |  compare  |    1       |         1         | compare w/ 0
+;; String  |    -1     |  compare   |        -1         | compare w/ ""
+;; List    |    -1     |    1       |      compare      | compare w/ [nil ...]
+;; Nil     | compare 0 | compare "" | compare [nil ...] |      -
+;;
+;; Additionally, there is a list of qualifiers with fixed ordering.
+
+(def ^:private QUALIFIERS
+  "Order Map for well-known Qualifiers."
+  { "alpha"     0 "a"         0
+    "beta"      1 "b"         1
+    "milestone" 2 "m"         2
+    "rc"        3 "cr"        3
+    "snapshot"  5
+    ""          6 "final"     6 "stable"    6 })
+
+(defn- version-component-type 
+  "Get Keyword Identifying Component Type."
+  [v]
+  (cond (nil? v)     :nil
+        (integer? v) :int
+        (string? v)  :str
+        :else        :list))
+
+(defn- version-string-compare
+  "Compare string components of a version vector."
+  [^String s0  ^String s1]
+  (let [m0 (get QUALIFIERS s0)
+        m1 (get QUALIFIERS s1)
+        cr (cond (and m0 m1) (compare m0 m1)
+                 m0 1
+                 m1 -1
+                 :else (compare s0 s1))]
+    (cond (neg? cr) -1
+          (pos? cr)  1
+          :else      0)))
+
+(defn- version-vector-compare
+  "Compare Version Vectors."
+  [v0 v1]
+  (let [types (map version-component-type [v0 v1])] 
+    (condp = types
+      [:list :list] (let [v0* (if (< (count v0) (count v1)) (concat v0 (repeat nil)) v0)
+                          v1* (if (< (count v1) (count v0)) (concat v1 (repeat nil)) v1)]
+                      (or
+                        (some
+                          (fn [[c0 c1]]
+                            (let [r (version-vector-compare c0 c1)]
+                              (when-not (zero? r)
+                                r)))
+                          (map vector v0* v1*))
+                        0))
+      [:int  :int]  (compare v0 v1)
+      [:int  :str]  1
+      [:int  :list] 1
+      [:int  :nil]  (if (zero? v0) 0 1)
+      [:str  :int]  -1
+      [:str  :str]  (version-string-compare v0 v1)
+      [:str  :list] -1
+      [:str  :nil]  (version-vector-compare v0 "")
+      [:list :int]  -1
+      [:list :str]  1
+      [:list :nil]  (version-vector-compare v0 (repeat (count v0) nil))
+      [:nil :int]   (if (zero? v1) 0 -1)
+      [:nil :str]   (version-vector-compare "" v1)
+      [:nil :list]  (version-vector-compare (repeat (count v1) nil) v1)
+      (throw (Exception. "Invalid Version Vector")))))
+
+;; ## Version Map
 
 (defn version-map
   "Create version map from version string. Uses everything after the first
-   non-number-or-dot character as `:qualifier`, storing a vector of version numbers
-   in `:version`."
+  non-number-or-dot character as `:qualifier`, storing a vector of version numbers
+  in `:version`."
   [^String version]
-  (let [^String v (first (.split version "[^0-9.]" 2))
-        ^String q (let [^String q (.substring version (count v))]
-                    (if (.startsWith q "-")
-                      (.substring q 1)
-                      q))
-        ^String q (when (seq q) (.toLowerCase q))]
-    (-> {}
-      (assoc :version-str version)
-      (assoc :qualifier q)
-      (assoc :version (map #(Integer/parseInt ^String %) (.split v "\\."))))))
-
-(defn- qualifier-compare
-  "Compare two qualifier strings. This tries to introduce numeric comparison
-   when using qualifiers like 'alpha5' and 'alpha12'"
-  [q0 q1]
-  (cond (= q0 q1)          0
-        (not q0)           1
-        (not q1)          -1
-        (= q0 "snapshot")  1
-        (= q1 "snapshot") -1
-        :else (or 
-                (when-let [[_ qa ra] (re-find (re-matcher #"^([a-zA-Z_-]*)([0-9]+)$" q0))]
-                  (when-let [[_ qb rb] (re-find (re-matcher #"^([a-zA-Z_-]*)([0-9]+)$" q1))]
-                    (when (= qa qb)
-                      (if (< (Integer/parseInt ra) (Integer/parseInt rb))
-                        -1
-                        1))))
-                (.compareTo q0 q1))))
+  (-> {}
+    (assoc :version-str version)
+    (assoc :version (version-split version))))
 
 (defn- version-map-compare
   "Compare two version maps."
   [m0 m1]
-  (let [v0* (:version m0)
-        v1* (:version m1)
-        v0 (if (< (count v0*) (count v1*)) (concat v0* (repeat 0)) v0*)
-        v1 (if (< (count v1*) (count v0*)) (concat v1* (repeat 0)) v1*)
-        version-zip (map vector v0 v1)
-        version-compare (if-let [[d0 d1] (some #(when-not (= (first %) (second %)) %) version-zip)]
-                          (compare d0 d1)
-                          0)]
-    (if (zero? version-compare)
-      (qualifier-compare (:qualifier m0) (:qualifier m1))
-      version-compare)))
+  (version-vector-compare (:version m0) (:version m1)))
 
 (defn version-outdated?
   "Check if the first version is outdated."
@@ -72,4 +160,9 @@
 (defn snapshot?
   "Check if the given version is a SNAPSHOT."
   [v]
-  (= (:qualifier v) "snapshot"))
+  (some
+    (fn [x]
+      (cond (string? x) (= x "snapshot")
+            (integer? x) nil
+            :else (snapshot? {:version x})))
+    (:version v)))
--- test/leiningen/version_test.clj
+++ test/leiningen/version_test.clj
@@ -6,17 +6,16 @@
   (fact "about version map creation"
     (let [m (version-map ?version)]
       (:version m) => ?v
-      (:qualifier m) => ?q
       (:version-str m) => ?version))
-  ?version         ?v           ?q
-  "1.0.0"          [1 0 0]      nil
-  "1.0"            [1 0]        nil
-  "1"              [1]          nil
-  "1a"             [1]          "a"
-  "1-a"            [1]          "a"
-  "1.0.1-SNAPSHOT" [1 0 1]      "snapshot"
-  "1.0.1-alpha2"   [1 0 1]      "alpha2"
-  "11.2.0.3.0"     [11 2 0 3 0] nil)
+  ?version         ?v
+  "1.0.0"          [1 0 0]
+  "1.0"            [1 0]
+  "1"              [1]
+  "1a"             [1 "a"]
+  "1-a"            [1 ["a"]]
+  "1.0.1-SNAPSHOT" [1 0 1 ["snapshot"]]
+  "1.0.1-alpha2"   [1 0 1 ["alpha" 2]]
+  "11.2.0.3.0"     [11 2 0 3 0])
 
 (fact "about SNAPSHOTs"
   (version-map "1.0.0") =not=> snapshot?
@@ -39,14 +38,24 @@
   "10.2.0.3.0"     "5.2.0.3.0"       1
   "1.0.0-SNAPSHOT" "1.0.1-SNAPSHOT" -1
   "1.0.0-alpha"    "1.0.1-beta"     -1
+  "1.1-dolphin"    "1.1.1-cobra"    -1
 
   ;; Lexical Comparison
+  "1.0-alpaca"     "1.0-bermuda"    -1
+  "1.0-alpaca"     "1.0-alpaci"     -1
+  "1.0-dolphin"    "1.0-cobra"       1
+
+  ;; Qualifier Comparison
   "1.0.0-alpha"    "1.0.0-beta"     -1
   "1.0.0-beta"     "1.0.0-alpha"     1
+  "1.0.0-alpaca"   "1.0.0-beta"     -1
+  "1.0.0-final"    "1.0.0-milestone" 1
 
-  ;; Lexical/Numeric Comparison
+  ;; Qualifier/Numeric Comparison
   "1.0.0-alpha1"   "1.0.0-alpha2"   -1
   "1.0.0-alpha5"   "1.0.0-alpha23"  -1
+  "1.0-RC5"        "1.0-RC20"       -1
+  "1.0-RC11"       "1.0-RC6"         1
 
   ;; Releases are newer than SNAPSHOTs
   "1.0.0"          "1.0.0-SNAPSHOT"  1
@@ -59,4 +68,10 @@
 
   ;; SNAPSHOTS are newer than qualified versions
   "1.0.0-SNAPSHOT" "1.0.0-RC1"       1
-  "1.0.0-SNAPSHOT" "1.0.1-RC1"      -1)
+  "1.0.0-SNAPSHOT" "1.0.1-RC1"      -1
+  
+  ;; Some other Formats
+  "9.1-901.jdbc4"   "9.1-901.jdbc3"   1
+  "9.1-901-1.jdbc4" "9.1-901.jdbc4"   1
+
+  )
