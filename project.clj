f5c06b694a54d021181b99ab5136b844f81a2b78
Yannick Scherer <yannick.scherer@gmail.com>
2013-07-26 23:33:18 +0200

    Added Midje.

--- project.clj
+++ project.clj
@@ -6,4 +6,12 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.xml "0.0.7"]
                  [clj-aws-s3 "0.3.6"]
-                 [version-clj "0.1.0"]])
+                 [version-clj "0.1.0"]]
+  :repositories  {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]
+                   :plugins [[lein-midje "3.0.1"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
+  :aliases {"midje-all" ["with-profile" "dev,1.4:dev,1.5:dev,1.6" "midje"]
+            "deps-all" ["with-profile" "dev,1.4:dev,1.5:dev,1.6" "deps"]})
