b25dc6609b0ce73f656da92478bccf2b1ff3f77c
Yannick Scherer <yannick.scherer@gmail.com>
2013-07-27 00:04:13 +0200

    Added first draft of ancient-clj.repository namespace.

---
+++ src/ancient_clj/repository.clj
@@ -0,0 +1,31 @@
+(ns ^{ :doc "Repository Access for ancient-clj."
+       :author "Yannick Scherer" }
+  ancient-clj.repository)
+
+;; ## Protocol
+
+(defprotocol Repository
+  (retrieve-metadata-xml! [this group-id artifact-id]
+    "Retrieve the version metadata XML file as a String."))
+
+(extend-protocol Repository
+  clojure.lang.AFunction
+  (retrieve-metadata-xml! [f group-id artifact-id]
+    (f group-id artifact-id))
+  nil
+  (retrieve-metadata-xml! [this group-id artifact-id]
+    nil))
+
+;; ## Registry
+
+(defmulti create-repository
+  "Create entity implementing the `Repository` protocol for the given
+   repository map (`:url`, `:credentials`, ...)"
+  (fn [{:keys [^String url] :as options}]
+    (when url
+      (let [i (.indexOf url ":/")]
+        (when-not (neg? i)
+          (.substring url 0 i)))))
+  :default nil)
+
+(defmethod create-repository nil [& _] nil)
