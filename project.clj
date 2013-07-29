0c134f34ca8f96487fa87a6c58b5ac2bbfdc1b90
Yannick Scherer <yannick.scherer@gmail.com>
2013-07-30 01:11:29 +0200

    Using [ancient-clj 0.1.1]

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :description "Check your Projects for outdated Dependencies."
   :url "https://github.com/xsc/lein-ancient"
   :dependencies [[rewrite-clj "0.2.0"]
-                 [ancient-clj "0.1.1-SNAPSHOT"]]
+                 [ancient-clj "0.1.1"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :profiles {:midje {:dependencies [[midje "1.5.1"]]
-                     :plugins [[lein-midje "3.0.1"]]
+                     :plugins [[lein-midje "3.1.1"]]
                      :test-paths ["test"]}}
   :aliases {"midje-dev" ["with-profile" "midje" "midje"]
             "deps-dev" ["with-profile" "midje" "deps"]}
