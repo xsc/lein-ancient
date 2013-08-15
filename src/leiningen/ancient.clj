0143bc231bab6b6d03525294f69dee1999e8d960
Yannick Scherer <yannick.scherer@gmail.com>
2013-08-15 16:02:42 +0200

    Added 'test' task (see #11). Updated docstring.

--- src/leiningen/ancient.clj
+++ src/leiningen/ancient.clj
@@ -4,12 +4,14 @@
   (:require [leiningen.ancient.tasks.check :refer [run-check-task!]]
             [leiningen.ancient.tasks.get :refer [run-get-task!]]
             [leiningen.ancient.tasks.upgrade :refer [run-upgrade-task! run-upgrade-global-task!]]
+            [leiningen.ancient.tasks.test :refer [run-test-task!]]
             [ancient-clj.verbose :refer :all]))
 
 (def ^:private dispatch-table
   {"get"            run-get-task!
    "upgrade"        run-upgrade-task!
    "upgrade-global" run-upgrade-global-task!
+   "test"           run-test-task!
    nil              run-check-task!})
 
 (defn ^:no-project-needed ancient
@@ -19,29 +21,33 @@
 
      lein ancient [<options>]
      lein ancient get <package> [<options>]
+     lein ancient test [<options>]
      lein ancient upgrade [<options>]
      lein ancient upgrade-global [<options>]
 
    Tasks:
 
      get                  Retrieve artifact information from Maven repositories.
+     test                 Auto-detect test framework (clojure.test, midje, ...) and run tests.
      upgrade              Replace artifacts in your 'project.clj' with newer versions.
      upgrade-global       Replace plugins in '~/.lein/profiles.clj' with newer versions.
 
    Commandline Options:
   
+     :aggressive          Check all available repositories (= Do not stop after first artifact match).
      :all                 Check Dependencies and Plugins.
-     :dependencies        Check Dependencies. (default)
-     :plugins             Check Plugins.
-     :no-profiles         Do not check Dependencies/Plugins in Profiles.
      :allow-qualified     Allow '*-alpha*' versions & co. to be reported as new.
      :allow-snapshots     Allow '*-SNAPSHOT' versions to be reported as new.
      :check-clojure       Include Clojure (org.clojure/clojure) in checks.
-     :aggressive          Check all available repositories (= Do not stop after first artifact match).
+     :dependencies        Check Dependencies. (default)
      :interactive         Run ':upgrade' in interactive mode, prompting whether to apply changes.
+     :no-colors           Disable colorized output.
+     :no-profiles         Do not check Dependencies/Plugins in Profiles.
+     :no-tests            Do not run tests after upgrading a project.
+     :overwrite-backup    Do not prompt if a backup file exists when upgrading a project.
+     :plugins             Check Plugins.
      :print               Print result of ':upgrade' task instead of writing it to 'project.clj'.
      :verbose             Produce progress indicating messages.
-     :no-colors           Disable colorized output.
   "
   [project & args]
   (let [^String t (when-let [^String t (first args)]
