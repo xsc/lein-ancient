ba98091828b49dd7d8550d6b384e68188e402a74
Yannick Scherer <yannick.scherer@gmail.com>
2013-09-18 15:29:06 +0200

    display warnings for invalid/deprecated flags. (closes #15)

--- src/leiningen/ancient/utils/cli.clj
+++ src/leiningen/ancient/utils/cli.clj
@@ -19,14 +19,22 @@
    ":no-tests"        :no-tests
    ":overwrite-backup":overwrite-backup})
 
+(def ^:private CLI_DEPRECATED
+  "Flags that are no longer supported."
+  {":upgrade"        "use task 'upgrade' instead"
+   ":upgrade-global" "use task 'upgrade-global' instead"
+   ":get"            "use task 'get' instead"})
+
 (defn parse-cli
   "Parse Command Line, return map of Settings."
   [args]
   (let [data (->> (for [^String flag args]
-                    (when (contains? CLI_FLAGS flag)
-                      (vector
-                        (get CLI_FLAGS flag)
-                        true)))
+                    (cond (contains? CLI_DEPRECATED flag) (println "WARN: option" (str "'" flag "'") "is no longer supported"
+                                                                   (str "(" (get CLI_DEPRECATED flag) ")."))
+                          (contains? CLI_FLAGS flag) (vector
+                                                       (get CLI_FLAGS flag)
+                                                       true)
+                          :else (println "WARN: option" (str "'" flag "'") "not recognized.")))
                (into {}))
         data (assoc data 
                     :aggressive? (:aggressive? data false)
