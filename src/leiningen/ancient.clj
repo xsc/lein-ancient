1ce25fb8a8ad4e46eac3b7cdf767f52a12b3201b
Yannick Scherer <yannick.scherer@gmail.com>
2013-09-28 22:32:06 +0200

    Added ':recursive' to help docstring.

--- src/leiningen/ancient.clj
+++ src/leiningen/ancient.clj
@@ -35,9 +35,9 @@
   
      :aggressive          Check all available repositories (= Do not stop after first artifact match).
      :all                 Check Dependencies and Plugins.
+     :allow-all           Allow SNAPSHOT and qualified versions to be reported as new.
      :allow-qualified     Allow '*-alpha*' versions & co. to be reported as new.
      :allow-snapshots     Allow '*-SNAPSHOT' versions to be reported as new.
-     :allow-all           Allow SNAPSHOT and qualified versions to be reported as new.
      :check-clojure       Include Clojure (org.clojure/clojure) in checks.
      :dependencies        Check Dependencies. (default)
      :interactive         Run 'upgrade' in interactive mode, prompting whether to apply changes.
@@ -46,7 +46,8 @@
      :no-tests            Do not run tests after upgrading a project.
      :overwrite-backup    Do not prompt if a backup file exists when upgrading a project.
      :plugins             Check Plugins.
-     :print               Print result of ':upgrade' task instead of writing it to 'project.clj'.
+     :print               Print result of 'upgrade' task instead of writing it to 'project.clj'.
+     :recursive           Perform recursive 'check' or 'upgrade'.
   "
   [project & args]
   (let [^String t (when-let [^String t (first args)]
