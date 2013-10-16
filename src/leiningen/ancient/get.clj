990dfcc499ad334cea4fba160b81e6eecb798d55
Yannick Scherer <yannick.scherer@stylefruits.de>
2013-10-16 15:57:35 +0200

    Refactored common parts in 'get' and 'latest' tasks.

--- src/leiningen/ancient/get.clj
+++ src/leiningen/ancient/get.clj
@@ -38,40 +38,45 @@
           (doseq [v vp] (print (pr-str v) ""))))
       (println "]"))))
 
-(defn run-get-task!
-  [project [package & args]]
-  (if-not package
-    (println "':get' expects an artifact to retrieve version information for.")
-    (let [settings (-> (parse-cli args) (assoc :aggressive? true))
-          {:keys [artifact-id group-id] :as artifact} (anc/artifact-map [package "RELEASE"])
-          artifact-str (str group-id "/" artifact-id)]
-      (with-output-settings settings
-        (let [repos (collect-repositories project)]
-          (println "Getting Version Information for" (yellow artifact-str) 
-                   "from" (count repos) "Repositories ...")
-          (let [vs (->> (anc/versions! settings repos artifact)
-                     (sort #(v/version-seq-compare (second %1) (second %2)))
-                     (reverse))]
-            (if-not (seq vs)
-              (println "No versions found.")
-              (let [releases (filter (complement (comp v/qualified? second)) vs)
-                    snapshots (filter (comp v/snapshot? second) vs)
-                    qualified (filter (comp  #(and (not (v/snapshot? %)) (v/qualified? %)) second) vs)]
-                (println (str "  * " (count vs) " version(s) found."))
-                (print-version "latest release" (first releases))
-                (print-version "latest SNAPSHOT" (first snapshots))
-                (print-version "latest qualified" (first qualified))
-                (print-version-seq "all releases" releases)
-                (print-version-seq "all SNAPSHOTs" snapshots)
-                (print-version-seq "all qualified versions" qualified)))))))))
+(defn- wrap-version-retrieval-task
+  [f task-name]
+  (let [error-msg (format "'%s' expects an artifact to retrieve version information for." task-name)]
+    (fn [project [package & args]]
+      (if-not package
+        (println error-msg)
+        (let [settings (assoc (parse-cli args) :aggressive? true)
+              artifact (anc/artifact-map [package "RELEASE"])]
+          (with-output-settings settings
+            (let [repos (collect-repositories project)]
+              (f settings repos package artifact))))))))
 
-(defn run-latest-vector-task!
-  [project [package & args]]
-  (if-not package
-    (println "Expected an artifact to retrieve version information for")
-    (let [settings (assoc (parse-cli args) :aggressive? true)
-          artifact (anc/artifact-map [package "RELEASE"])]
-      (with-output-settings settings
-        (if-let [latest (retrieve-latest-version-string! settings (collect-repositories project) (:group-id artifact) (:artifact-id artifact))]
-          (println (str  "[" package " \"" latest "\"]") )
-          (println "No versions found"))))))
+(def run-get-task!
+  (->
+    (fn [settings repos package {:keys [group-id artifact-id] :as artifact}]
+      (let [artifact-str (str group-id "/" artifact-id)] 
+        (println "Getting Version Information for" (yellow artifact-str) 
+                 "from" (count repos) "Repositories ...")
+        (let [vs (->> (anc/versions! settings repos artifact)
+                      (sort #(v/version-seq-compare (second %1) (second %2)))
+                      (reverse))]
+          (if-not (seq vs)
+            (println "No versions found.")
+            (let [releases (filter (complement (comp v/qualified? second)) vs)
+                  snapshots (filter (comp v/snapshot? second) vs)
+                  qualified (filter (comp  #(and (not (v/snapshot? %)) (v/qualified? %)) second) vs)]
+              (println (str "  * " (count vs) " version(s) found."))
+              (print-version "latest release" (first releases))
+              (print-version "latest SNAPSHOT" (first snapshots))
+              (print-version "latest qualified" (first qualified))
+              (print-version-seq "all releases" releases)
+              (print-version-seq "all SNAPSHOTs" snapshots)
+              (print-version-seq "all qualified versions" qualified))))))
+    (wrap-version-retrieval-task "get")))
+
+(def run-latest-vector-task!
+  (->
+    (fn [settings repos package {:keys [group-id artifact-id]}]
+      (if-let [latest (retrieve-latest-version-string! settings repos group-id artifact-id)]
+        (println (format "[%s \"%s\"]" package latest))
+        (println "No versions found.")))
+    (wrap-version-retrieval-task "latest")))
