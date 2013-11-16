e255c8984a58f4acb9a10ff0d944db70ec51d7ba
Yannick Scherer <yannick.scherer@stylefruits.de>
2013-11-16 13:24:30 +0100

    Release 0.1.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ancient-clj "0.1.5-SNAPSHOT"
+(defproject ancient-clj "0.1.5"
   :description "Maven Version Utilities for Clojure"
   :url "https://github.com/xsc/ancient-clj"
   :license {:name "Eclipse Public License"
