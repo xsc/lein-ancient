f7f88a242e9e4e8f079f3f706a1fee7f99b878bc
Yannick Scherer <yannick.scherer@stylefruits.de>
2013-12-05 10:23:22 +0100

    Using Midje 1.6.0

--- project.clj
+++ project.clj
@@ -7,13 +7,11 @@
                  [org.clojure/data.xml "0.0.7"]
                  [colorize "0.1.1" :exclusions [org.clojure/clojure]]
                  [version-clj "0.1.0" :exclusions [org.clojure/clojure]]
-                 [clj-aws-s3 "0.3.7" :exclusions [org.clojure/clojure org.apache.httpcomponents/httpclient commons-codec]]
-                 [org.apache.httpcomponents/httpclient "4.3.1"]
-                 [commons-codec "1.8"]]
+                 [clj-aws-s3 "0.3.7" :exclusions [org.clojure/clojure clj-time]]
+                 [clj-time "0.6.0" :exclusions [org.clojure/clojure]]]
   :repositories  {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :profiles {:dev {:dependencies [[midje "1.5.1"]
-                                  [org.codehaus.plexus/plexus-utils "3.0.15"]]
-                   :plugins [[lein-midje "3.1.1"]]}
+  :profiles {:dev {:dependencies [[midje "1.6.0" :exclusions [clj-time]]]
+                   :plugins [[lein-midje "3.1.3"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
