6bc1c9fc0091ec469187e7353bad7e7f4d981655
Yannick Scherer <yannick.scherer@gmail.com>
2013-12-14 12:46:21 +0100

    upgrade dependencies, use 'pedantic?'

--- project.clj
+++ project.clj
@@ -2,12 +2,13 @@
   :description "Check your Projects for outdated Dependencies."
   :url "https://github.com/xsc/lein-ancient"
   :dependencies [[rewrite-clj "0.3.4"]
-                 [ancient-clj "0.1.5"]]
+                 [ancient-clj "0.1.6"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:midje {:dependencies [[midje "1.5.1"]]
+  :profiles {:midje {:dependencies [[midje "1.6.0"]]
                      :plugins [[lein-midje "3.1.1"]]
                      :test-paths ["test"]}}
   :aliases {"midje-dev" ["with-profile" "midje" "midje"]
             "deps-dev" ["with-profile" "midje" "deps"]}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :pedantic? :abort)
