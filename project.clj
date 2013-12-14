45dc889025040db387be6b4df14d220666060b0a
Yannick Scherer <yannick.scherer@gmail.com>
2013-12-14 13:05:25 +0100

    cleaned up exclusions; modified test alias

--- project.clj
+++ project.clj
@@ -5,17 +5,17 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.xml "0.0.7"]
-                 [colorize "0.1.1" :exclusions [org.clojure/clojure]]
-                 [version-clj "0.1.0" :exclusions [org.clojure/clojure]]
-                 [clj-aws-s3 "0.3.7" :exclusions [org.clojure/clojure clj-time commons-codec]]
-                 [clj-time "0.6.0" :exclusions [org.clojure/clojure]]
+                 [colorize "0.1.1"]
+                 [version-clj "0.1.0"]
+                 [clj-aws-s3 "0.3.7"]
+                 [clj-time "0.6.0"]
                  [commons-codec "1.6"]]
   :repositories  {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :profiles {:dev {:dependencies [[midje "1.6.0" :exclusions [clj-time commons-codec]]]
-                   :plugins [[lein-midje "3.1.3"]]}
+                   :plugins [[lein-midje "3.1.3"]]
+                   :exclusions [org.clojure/clojure]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
-  :aliases {"midje-dev" ["with-profile" "dev,1.4:dev,1.5:dev,1.6" "midje"]
-            "deps-dev" ["with-profile" "dev,1.4:dev,1.5:dev,1.6" "deps"]}
+  :aliases {"all" ["with-profile" "dev,1.4:dev,1.5:dev,1.6"]}
   :pedantic? :abort)
