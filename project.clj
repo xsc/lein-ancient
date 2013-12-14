2bae2a3a0e9df57d8171d340f23fbe42656713cd
Yannick Scherer <yannick.scherer@gmail.com>
2013-12-14 13:14:33 +0100

    Back to SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-ancient "0.5.4"
+(defproject lein-ancient "0.5.5-SNAPSHOT"
   :description "Check your Projects for outdated Dependencies."
   :url "https://github.com/xsc/lein-ancient"
   :dependencies [[rewrite-clj "0.3.5"]
