fcf26f2a0724298d5ce905d4d145bebc1e0e4f00
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-02-09 21:00:13 +0100

    use [ancient-clj "0.1.8"]

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Check your Projects for outdated Dependencies."
   :url "https://github.com/xsc/lein-ancient"
   :dependencies [[rewrite-clj "0.3.7"]
-                 [ancient-clj "0.1.7"]]
+                 [ancient-clj "0.1.8"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :profiles {:dev {:dependencies [[midje "1.6.0"]]
