ca2ed3537fa998231ba57a4c68c9d52aa29b6422
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-06-09 23:27:13 +0200

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject lein-ancient "0.5.6-SNAPSHOT"
   :description "Check your Projects for outdated Dependencies."
   :url "https://github.com/xsc/lein-ancient"
-  :dependencies [[rewrite-clj "0.3.8"]
+  :dependencies [[rewrite-clj "0.3.9"]
                  [ancient-clj "0.1.9"]
                  [jansi-clj "0.1.0"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[midje "1.6.0"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]
                    :plugins [[lein-midje "3.1.1"]]
                    :test-paths ["test"]}}
   :eval-in-leiningen true
