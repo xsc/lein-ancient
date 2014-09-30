eaef71fa973d84fd839f13a32503316eb7608f96
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-09-30 12:02:49 +0200

    Bump to 0.2.0-SNAPSHOT; upgrade dependencies.

--- project.clj
+++ project.clj
@@ -1,24 +1,26 @@
-(defproject ancient-clj "0.1.11-SNAPSHOT"
+(defproject ancient-clj "0.2.0-SNAPSHOT"
   :description "Maven Version Utilities for Clojure"
   :url "https://github.com/xsc/ancient-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.xml "0.0.7"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.xml "0.0.8"]
                  [colorize "0.1.1"]
                  [version-clj "0.1.0"]
-                 [clj-aws-s3 "0.3.7"]
-                 [clj-time "0.6.0"]
-                 [clj-http "0.7.9"]
+                 [clj-aws-s3 "0.3.10"]
+                 [clj-time "0.8.0"]
+                 [clj-http "1.0.0"]
+                 [com.fasterxml.jackson.core/jackson-core "2.4.2"]
+                 [com.fasterxml.jackson.core/jackson-annotations "2.4.2"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.4.2"]
+                 [joda-time "2.4"]
                  [commons-codec "1.9"]
-                 [commons-logging "1.1.3"]]
-  :exclusions [org.clojure/clojure]
+                 [commons-logging "1.2"]]
+  :exclusions [org.clojure/clojure joda-time]
   :repositories  {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :profiles {:dev {:dependencies [[midje "1.6.0" :exclusions [clj-time commons-codec]]]
+  :profiles {:dev {:dependencies [[midje "1.6.3" :exclusions [clj-time commons-codec]]]
                    :plugins [[lein-midje "3.1.3"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
-  :aliases {"all"          ["with-profile" "dev,1.4:dev,1.5:dev,1.6"]
-            "test-ancient" ["with-profile" "dev,1.4:dev,1.5:dev,1.6" "midje"]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+  :aliases {"all"  ["with-profile" "dev:+1.5"]
+            "test" ["midje"]}
   :pedantic? :abort)
