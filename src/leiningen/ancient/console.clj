f4dd4f5d797b7be9753b1a53355451defec8bda9
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-10-18 19:22:24 +0200

    add prompt/print functions.

---
+++ src/leiningen/ancient/console.clj
@@ -0,0 +1,31 @@
+(ns leiningen.ancient.console
+  (:require [leiningen.ancient.verbose :refer :all]
+            [jansi-clj.core :as color]))
+
+(defn prompt
+  "Create a yes/no prompt using the given message."
+  [& msg]
+  (let [msg (str (apply str msg) " [yes/no] ")]
+    (locking *out*
+      (loop [i 0]
+        (when (= (mod i 4) 2)
+          (println "*** please type in one of 'yes'/'y' or 'no'/'n' ***"))
+        (print msg)
+        (flush)
+        (let [r (or (read-line) "")
+              r (.toLowerCase ^String r)]
+          (case r
+            ("yes" "y") true
+            ("no" "n")  false
+            (recur (inc i))))))))
+
+(defn print-outdated-message
+  "Print a message indicating that a given artifact is outdated."
+  [{:keys [latest artifact]}]
+  {:pre [(map? latest) (map? artifact)]}
+  (let [{:keys [symbol version-string]} artifact]
+    (verbosef
+      "[%s %s] is available but we use %s"
+      symbol
+      (color/green (pr-str (:version-string latest)))
+      (color/yellow (pr-str version-string)))))
