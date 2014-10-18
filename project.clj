120ae03be716f524e3107b93cae969abef0facea
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-10-18 19:16:05 +0200

    upgrade dependencies; simplify test task.

--- project.clj
+++ project.clj
@@ -2,14 +2,16 @@
   :description "Check your Projects for outdated Dependencies."
   :url "https://github.com/xsc/lein-ancient"
   :dependencies [[rewrite-clj "0.3.9"]
-                 [ancient-clj "0.1.10"]
+                 [ancient-clj "0.2.0"]
                  [jansi-clj "0.1.0"]
+                 [pandect "0.4.1"]
+                 [potemkin "0.3.10"]
                  [commons-io "2.4"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:test {:dependencies [[midje "1.6.3"]]
-                    :plugins [[lein-midje "3.1.1"]]
-                    :test-paths ["test"]}}
-  :aliases {"test" ["with-profile" "+test" "midje"]}
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+                   :plugins [[lein-midje "3.1.1"]]
+                   :test-paths ["test"]}}
+  :aliases {"test" ["midje"]}
   :eval-in-leiningen true
   :pedantic? :abort)
