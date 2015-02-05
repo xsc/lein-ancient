9c20f7d7bf6a3486310ac6dbe6ee3a99df5f8c2a
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-02-05 16:23:03 +0100

    Version 0.6.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-ancient "0.6.2"
+(defproject lein-ancient "0.6.3-SNAPSHOT"
   :description "Check your Projects for outdated Dependencies."
   :url "https://github.com/xsc/lein-ancient"
   :dependencies [[rewrite-clj "0.4.11"]
