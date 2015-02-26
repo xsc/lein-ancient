4731f7426d8c36e2cf80bb0e3cd12e3197f6d8a8
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-02-26 14:48:29 +0100

    Version 0.3.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ancient-clj "0.3.3"
+(defproject ancient-clj "0.3.4-SNAPSHOT"
   :description "Maven Version Utilities for Clojure"
   :url "https://github.com/xsc/ancient-clj"
   :license {:name "Eclipse Public License"
