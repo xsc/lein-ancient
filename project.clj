2d8b8aa3c92058b7d5f1aa652b8676c6b312e9a7
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-04-08 09:43:53 +0200

    switch to MIT License.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject ancient-clj "0.3.7-SNAPSHOT"
   :description "Maven Version Utilities for Clojure"
   :url "https://github.com/xsc/ancient-clj"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "http://xsc.mit-license.org"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.xml "0.0.8"]
                  [version-clj "0.1.2"]
