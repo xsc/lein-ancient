8dfb2d27c7dd4bd8bbb8c63684021b5350625c8f
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-07-30 19:12:23 +0200

    Version 0.3.10

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ancient-clj "0.3.10-SNAPSHOT"
+(defproject ancient-clj "0.3.10"
   :description "Maven Version Utilities for Clojure"
   :url "https://github.com/xsc/ancient-clj"
   :license {:name "MIT License"
