26bd4f9ace2dea30b134ead53d84a7d1050a65e9
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-08-12 17:11:43 +0200

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -5,24 +5,24 @@
             :url "http://opensource.org/licenses/MIT"
             :year 2013
             :key "mit"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.reader "0.9.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.reader "0.9.2"]
                  [org.clojure/data.xml "0.0.8"]
                  [version-clj "0.1.2"]
                  [clj-aws-s3 "0.3.10" :exclusions [com.amazonaws/aws-java-sdk]]
-                 [com.amazonaws/aws-java-sdk-s3 "1.9.31"]
-                 [com.fasterxml.jackson.core/jackson-core "2.5.2"]
-                 [clj-time "0.9.0"]
-                 [clj-http "1.1.0"
+                 [com.amazonaws/aws-java-sdk-s3 "1.10.10"]
+                 [com.fasterxml.jackson.core/jackson-core "2.6.1"]
+                 [clj-time "0.10.0"]
+                 [clj-http "2.0.0"
                   :exclusions [com.cognitect/transit-clj
                                crouton
                                slingshot]]
-                 [joda-time "2.7"]
+                 [joda-time "2.8.2"]
                  [commons-codec "1.10"]
                  [commons-logging "1.2"]
-                 [potemkin "0.3.13"]]
+                 [potemkin "0.4.1"]]
   :exclusions [org.clojure/clojure joda-time]
-  :profiles {:dev {:dependencies [[midje "1.6.3" :exclusions [clj-time commons-codec]]
+  :profiles {:dev {:dependencies [[midje "1.7.0" :exclusions [clj-time commons-codec]]
                                   [http-kit "2.1.19"]]
                    :plugins [[lein-midje "3.1.3"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
