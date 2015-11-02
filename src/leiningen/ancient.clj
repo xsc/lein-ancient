06c8f66a5d0370c861c7c9782848f2c860dee690
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-11-02 12:48:55 +0100

    add warning to deprecated subtasks.

--- src/leiningen/ancient.clj
+++ src/leiningen/ancient.clj
@@ -4,7 +4,18 @@
              [get :as g]
              [upgrade :as u]
              [verbose :refer :all]]
+            [leiningen.core.main :as main]
             [jansi-clj.auto]))
+
+
+(defn- as-deprecated
+  [given-task new-task f arg]
+  (main/warn "WARN: the subtask"
+             (str "'" given-task "'")
+             "is deprecated, use"
+             (str "'" new-task "'")
+             "instead.\n")
+  (f arg))
 
 (defn
   ^:higher-order ^:no-project-needed
@@ -17,11 +28,14 @@
   ancient
   "Check your projects and profiles for outdated dependencies/plugins."
   [project & args]
-  (let [run #(apply % project (rest args))]
+  (let [run #(apply % project (rest args))
+        run-deprecated #(as-deprecated (first args) %1 run %2)]
     (case (first args)
       "check"                       (run c/check)
-      ("check-profiles" "profiles") (run c/check-profiles)
-      ("show-versions" "get")       (run g/show-versions)
+      "check-profiles"              (run c/check-profiles)
+      "get"                         (run-deprecated "show-versions" g/show-versions)
+      "profiles"                    (run-deprecated "check-profiles" c/check-profiles)
+      "show-versions"               (run g/show-versions)
       ("show-latest" "latest")      (run g/show-latest)
       "upgrade"                     (run u/upgrade)
       "upgrade-profiles"            (run u/upgrade-profiles)
