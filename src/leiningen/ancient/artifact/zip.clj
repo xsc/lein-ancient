7557ccf0f02d1b8c7170437d24a5d0b00426098d
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-03-25 21:44:37 +0100

    move through metadata nodes when upgrading artifacts (closes #67).

--- src/leiningen/ancient/artifact/zip.clj
+++ src/leiningen/ancient/artifact/zip.clj
@@ -27,18 +27,33 @@
 
 ;; ## Upgrade
 
+(defn- move-through-meta
+  [zloc]
+  (if (and zloc (#{:meta :meta*} (z/tag zloc)))
+    (recur (some-> zloc z/down z/right))
+    zloc))
+
+(defn- move-up-through-meta
+  [zloc]
+  (let [zloc (z/up zloc)]
+    (if (and zloc (#{:meta :meta*} (z/tag zloc)))
+      (recur zloc)
+      zloc)))
+
 (defn- move-to-index
   [zloc p]
-  (nth
-    (->> (iterate z/right zloc)
-         (remove #(= (z/tag %) :uneval)))
-    p))
+  (move-through-meta
+    (nth
+      (->> (iterate z/right zloc)
+           (remove #(= (z/tag %) :uneval)))
+      p)))
 
 (defn- move-to-value
   [zloc p]
-  (when-let [floc (z/find-value zloc p)]
-    (when-let [floc (z/right floc)]
-      floc)))
+  (some-> zloc
+          (z/find-value p)
+          (z/right)
+          (move-through-meta)))
 
 (defn- move-to-value-or-index
   "Move within a zipper. A keyword prompts descend into a map, a number moves
@@ -59,12 +74,14 @@
   [loc path new-version-string]
   {:pre [(integer? (last path))]}
   (or
-    (when-let [vloc (some-> (move-path loc path) z/down)]
+    (when-let [vloc (some-> (move-path loc path)
+                            z/down)]
       (-> (->> (if-let [sloc (z/right vloc)]
                  (z/replace sloc new-version-string)
                  (z/insert-right vloc new-version-string))
-               (iterate z/up))
-          (nth (inc (count path)))
+               (z/up)
+               (iterate move-up-through-meta))
+          (nth (count path))
           z/down))
     loc))
 
