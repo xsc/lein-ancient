d233a71bc150d6dfa806c8558a643a4f40ef20ee
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-03-27 12:46:43 +0200

    Version 0.6.10-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-ancient "0.6.9"
+(defproject lein-ancient "0.6.10-SNAPSHOT"
   :description "Check your Projects for outdated Dependencies."
   :url "https://github.com/xsc/lein-ancient"
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
