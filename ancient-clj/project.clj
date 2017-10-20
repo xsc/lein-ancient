f3ae5750c9dd37213d315161628ceccc4fbb32b7
Yannick Scherer <yannick.scherer@futurice.com>
2017-10-20 15:28:53 +0200

    Version 0.6.14-SNAPSHOT

--- ancient-clj/project.clj
+++ ancient-clj/project.clj
@@ -1,4 +1,4 @@
-(defproject ancient-clj "0.6.13"
+(defproject ancient-clj "0.6.14-SNAPSHOT"
   :description "Maven Version Utilities for Clojure"
   :url "https://github.com/xsc/ancient-clj"
   :license {:name "MIT License"
--- lein-ancient/project.clj
+++ lein-ancient/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-ancient "0.6.13"
+(defproject lein-ancient "0.6.14-SNAPSHOT"
   :description "Check your Projects for outdated Dependencies."
   :url "https://github.com/xsc/lein-ancient"
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
