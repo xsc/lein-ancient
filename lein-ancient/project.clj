e40cb52dbb6bc1796c97e495fff89aa5788dcc84
Yannick Scherer <yannick.scherer@futurice.com>
2017-12-14 10:22:12 +0100

    Version 0.6.15

--- lein-ancient/project.clj
+++ lein-ancient/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-ancient "0.6.15-SNAPSHOT"
+(defproject lein-ancient "0.6.15"
   :description "Check your Projects for outdated Dependencies."
   :url "https://github.com/xsc/lein-ancient"
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
@@ -10,7 +10,7 @@
                  ^:source-dep [potemkin "0.4.4"]
                  ^:source-dep [version-clj "0.1.2"]
                  ^:source-dep [jansi-clj "0.1.0"]
-                 ^:source-dep [ancient-clj "0.6.14"
+                 ^:source-dep [ancient-clj "0.6.15"
                                :exclusions [com.amazonaws/aws-java-sdk-s3
                                             clj-http]]
 
