a21e17441932839e13590df36a8ea7d616ace07e
Daniel Compton <desk@danielcompton.net>
2018-12-07 13:49:37 +1300

    Pin version of rrb-vector until lein-monolith upgrades

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject lein-ancient-monolith "MONOLITH"
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [[lein-monolith "1.0.1"]]
+  :plugins [[lein-monolith "1.0.1"]
+            ;; Add RRB vector manually until lein-monolith releases
+            ;; https://github.com/amperity/lein-monolith/pull/39
+            [org.clojure/core.rrb-vector "0.0.13"]]
   :monolith
   {:project-dirs ["ancient-clj" "lein-ancient"]})
