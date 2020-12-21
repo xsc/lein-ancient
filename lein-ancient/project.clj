0e182db80f092819620be191a17af6ba1d9f2130
Yannick Scherer <yannick.scherer@futurice.com>
2020-12-21 13:50:52 +0100

    Upgrade dependencies

--- lein-ancient/project.clj
+++ lein-ancient/project.clj
@@ -1,20 +1,20 @@
 (defproject lein-ancient "0.7.0-SNAPSHOT"
   :description "Check your Projects for outdated Dependencies."
   :url "https://github.com/xsc/lein-ancient"
-  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
 
                  ;; often-used dependencies to include as source
                  ;; (to avoid dependency conflicts)
-                 ^:source-dep [rewrite-clj "0.4.13"]
-                 ^:source-dep [org.clojure/tools.reader "0.10.0"]
-                 ^:source-dep [potemkin "0.4.4"]
+                 ^:source-dep [rewrite-clj "0.6.1"]
+                 ^:source-dep [org.clojure/tools.reader "1.3.4"]
+                 ^:source-dep [potemkin "0.4.5"]
                  ^:source-dep [version-clj "0.1.2"]
-                 ^:source-dep [jansi-clj "0.1.0"]
-                 ^:source-dep [ancient-clj "0.6.15"
+                 ^:source-dep [jansi-clj "0.1.1"]
+                 ^:source-dep [ancient-clj "0.7.0-SNAPSHOT"
                                :exclusions [com.amazonaws/aws-java-sdk-s3
                                             clj-http]]
 
-                 [clj-http "3.7.0"
+                 [clj-http "3.11.0"
                   :exclusions [commons-codec
                                org.apache.httpcomponents/httpcore]]
 
@@ -28,9 +28,10 @@
             :year 2013
             :key "mit"}
   :scm {:dir ".."}
-  :profiles {:dev {:dependencies [[midje "1.8.3"]
-                                  [slingshot "0.12.2"]]
-                   :plugins [[lein-midje "3.1.1"]]
+  :profiles {:dev {:dependencies [[midje "1.9.9"]
+                                  [slingshot "0.12.2"]
+                                  [fipp "0.6.23"]]
+                   :plugins [[lein-midje "3.2.2"]]
                    :test-paths ["test"]}}
   :aliases {"test" ["midje"]}
 
