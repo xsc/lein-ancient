17717d37e1bf5f1b1f31fb6e534a8ba7884c0032
Yannick Scherer <yannick.scherer@futurice.com>
2020-12-21 12:27:25 +0100

    Upgrade dependencies

--- ancient-clj/project.clj
+++ ancient-clj/project.clj
@@ -5,22 +5,22 @@
             :url "https://opensource.org/licenses/MIT"
             :year 2013
             :key "mit"}
-  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
                  [org.clojure/data.xml "0.2.0-alpha6"]
                  [version-clj "0.1.2"]
                  ;; Note that this is the samve version used by s3-wagon-private 1.3.0
                  [com.amazonaws/aws-java-sdk-s3 "1.11.28"]
-                 [clj-http "2.1.0"
+                 [clj-http "3.11.0"
                   :exclusions [com.cognitect/transit-clj
                                crouton
                                org.apache.httpcomponents/httpclient]]
                  [commons-logging "1.2"]
-                 [joda-time "2.9.2"]
-                 [potemkin "0.4.3"]]
+                 [joda-time "2.10.8"]
+                 [potemkin "0.4.5"]]
   :scm {:dir ".."}
-  :profiles {:dev {:dependencies [[midje "1.8.3"]
-                                  [clj-time "0.11.0"]
-                                  [http-kit "2.1.19"]]
-                   :plugins [[lein-midje "3.1.3"]]}}
+  :profiles {:dev {:dependencies [[midje "1.9.9"]
+                                  [clj-time "0.15.2"]
+                                  [http-kit "2.5.0"]]
+                   :plugins [[lein-midje "3.2.2"]]}}
   :aliases {"test" ["midje"]}
   :pedantic? :abort)
