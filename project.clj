28d54811d6d71bdbec03de80e180934d432e1738
Yannick Scherer <yannick.scherer@futurice.com>
2020-12-21 14:23:43 +0100

    Remove unused files

--- project.clj
+++
@@ -1,5 +0,0 @@
-(defproject lein-ancient-monolith "MONOLITH"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [[lein-monolith "1.0.1"]]
-  :monolith
-  {:project-dirs ["ancient-clj" "lein-ancient"]})
