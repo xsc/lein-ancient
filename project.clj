816928d19a692d0dcaf5546c55fb395d0764cedc
Yannick Scherer <yannick.scherer@futurice.com>
2021-02-13 16:15:24 +0100

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
             :url "https://choosealicense.com/licenses/mit"
             :year 2013
             :key "mit"}
-  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.10.2" :scope "provided"]
 
                  ;; often-used dependencies to include as source
                  ;; (to avoid dependency conflicts)
                  ^:inline-dep [rewrite-clj "0.6.1"]
-                 ^:inline-dep [org.clojure/tools.reader "1.3.4"]
+                 ^:inline-dep [org.clojure/tools.reader "1.3.5"]
                  ^:inline-dep [potemkin "0.4.5"]
                  ^:inline-dep [version-clj "1.0.0"]
                  ^:inline-dep [jansi-clj "0.1.1"]
@@ -25,18 +25,23 @@
                   :scope "provided"
                   :exclusions [org.apache.httpcomponents/httpclient
                                org.apache.httpcomponents/httpcore
-                               org.clojure/clojure]]
+                               org.clojure/clojure]
+                  :upgrade false]
 
                  ;; pin dependencies to match Leiningen
                  [commons-codec "1.11"
-                  :scope "provided"]
+                  :scope "provided"
+                  :upgrade false]
                  [commons-io "2.8.0"
-                  :scope "provided"]
+                  :scope "provided"
+                  :upgrade false]
                  [org.apache.httpcomponents/httpclient "4.5.13"
-                  :scope "provided"]
+                  :scope "provided"
+                  :upgrade false]
                  [org.clojure/data.xml "0.2.0-alpha5"
                   :exclusions [org.clojure/clojure]
-                  :scope "provided"]]
+                  :scope "provided"
+                  :upgrade false]]
 
   :plugins [[lein-isolate "0.2.1"]]
   :middleware [leiningen.isolate/middleware]
