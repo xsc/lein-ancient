c635b3183af81d462c8376cd3366b01809570795
Yannick Scherer <yannick.scherer@futurice.com>
2021-02-13 16:09:25 +0100

    Use stable versions of 'ancient-clj' and 'version-clj'

--- project.clj
+++ project.clj
@@ -13,9 +13,9 @@
                  ^:inline-dep [rewrite-clj "0.6.1"]
                  ^:inline-dep [org.clojure/tools.reader "1.3.4"]
                  ^:inline-dep [potemkin "0.4.5"]
-                 ^:inline-dep [version-clj "0.1.2"]
+                 ^:inline-dep [version-clj "1.0.0"]
                  ^:inline-dep [jansi-clj "0.1.1"]
-                 ^:inline-dep [ancient-clj "1.0.0-SNAPSHOT"
+                 ^:inline-dep [ancient-clj "1.0.0"
                                :exclusions [clj-commons/pomegranate
                                             potemkin
                                             riddley]]
