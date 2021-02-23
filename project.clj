ddcb0a1d655eda37daa47bea63e5eee72985a1ed
Yannick Scherer <yannick.scherer@futurice.com>
2021-02-23 10:53:58 +0100

    Upgrade 'version-clj'

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  ^:inline-dep [rewrite-clj "0.6.1"]
                  ^:inline-dep [org.clojure/tools.reader "1.3.5"]
                  ^:inline-dep [potemkin "0.4.5"]
-                 ^:inline-dep [version-clj "2.0.0"]
+                 ^:inline-dep [version-clj "2.0.1"]
                  ^:inline-dep [jansi-clj "0.1.1"]
                  ^:inline-dep [ancient-clj "1.0.0"
                                :exclusions [clj-commons/pomegranate
